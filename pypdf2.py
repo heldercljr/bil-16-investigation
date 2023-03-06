0d3f938ee8868d9a406c2cf0ea54d1b50bdded34
heldercljr <helder.junior@ccc.ufcg.edu.br>
2023-03-06 10:17:05 -0300

    Adição dos comentários.

--- pypdf2.py
+++ pypdf2.py
@@ -1,14 +1,27 @@
 import PyPDF2
 import re
 
+# Abertura do documento
+
 with open("conta3.pdf", "rb") as conta:
+
+    # Leitura do arquivo pela biblioteca
+
     pdf = PyPDF2.PdfReader(conta)
+
+    # Seleção da página do documento
 
     page1 = pdf.pages[0]
 
+    # Extração do texto da página
+
     text = page1.extract_text()
 
+    # Separação do texto por linha
+
     lines = text.split("\n")
+
+    # Dicionário responsável pela exportação dos dados
 
     report = {
         "valor": lines[7].split()[0],
@@ -16,5 +29,7 @@ with open("conta3.pdf", "rb") as conta:
         "codigo_de_barras": re.sub(r'\D', '', lines[-8]),
         "tipo_do_documento": lines[-7].strip()
     }
+
+    # Demonstração dos dados
 
     print(report)
--- pypdf4.py
+++ pypdf4.py
@@ -1,14 +1,27 @@
 import PyPDF4
 import re
 
+# Abertura do documento
+
 with open("conta4.pdf", "rb") as conta:
+
+    # Leitura do arquivo pela biblioteca
+
     pdf = PyPDF4.PdfFileReader(conta)
-    
+
+    # Seleção da página do documento
+
     page1 = pdf.getPage(0)
+
+    # Extração do texto da página
 
     text = page1.extractText()
 
+    # Separação do texto por linha
+
     lines = text.split("\n")
+
+    # Dicionário responsável pela exportação dos dados
 
     report = {
         "valor": lines[12],
@@ -16,5 +29,7 @@ with open("conta4.pdf", "rb") as conta:
         "codigo_de_barras": re.sub(r'\D', '', lines[-17]) + lines[-16],
         "tipo_do_documento": lines[-14].replace("!", "ã")
     }
+
+    # Demonstração dos dados
 
     print(report)
