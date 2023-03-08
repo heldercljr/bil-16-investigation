2569e98954406d52f90e0393d47a3af8ba95934c
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2023-03-08 18:17:46 -0300

    Concluído o script que utiliza PyMuPDF para gerar o relatório em JSON com todos os atributos importantes.

--- pymupdf_.py
+++ pymupdf_.py
@@ -1,10 +1,11 @@
 import fitz
+import re
 
 from report import Report
 
 # Nome do documento de onde virão os dados a serem extraídos
 
-filename = "conta3.pdf"
+filename = "conta4.pdf"
 
 # Abertura do documento pela própria biblioteca
 
@@ -18,6 +19,12 @@ with fitz.open(filename) as conta:
 
 lines = text.split("\n")
 
+# Lista com a composição do documento:
+
+lista = lines[lines.index("Composição do Documento de Arrecadação") + 1:lines.index("Totais"):1]
+lista = [lista[i-1] for i in range(1, len(lista)) if lista[i-1].isupper() or re.match(r'\d+(,\d{2})', lista[i])]
+lista = [{"denominacao": lista[i], "valor": lista[i+1]} for i in range(0, len(lista), 2)]
+
 # Objeto responsável pela exportação dos dados
 
 report = Report(
@@ -26,7 +33,7 @@ report = Report(
     lines[9],
     "".join(lines[-20:-16:1]).replace(" ", ""),
     lines[-15],
-    lines[24:52:1]
+	lista
 )
 
 # Exportação do relatório para um arquivo JSON
